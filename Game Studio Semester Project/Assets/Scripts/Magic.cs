7ba48a89219b621e0bb164772486f3242c90a991
Auroralain <104611153+Auroralain@users.noreply.github.com>
2023-04-05 12:42:26 +0800

    Revert "1"
    This reverts commit db0519be8a1481a6a563bd20e78e750aa14263b8.

--- Game Studio Semester Project/Assets/Scripts/Magic.cs
+++ Game Studio Semester Project/Assets/Scripts/Magic.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Magic : MonoBehaviour
 {
+    public Transform lightMagic;
     public GameObject lightballPrefab;
-    private GameObject go;
 
     void Update()
     {
@@ -17,15 +17,7 @@ public class Magic : MonoBehaviour
     }
     void Spell()
     {
-<<<<<<< HEAD
-        go = Instantiate(lightballPrefab, lightMagic.position, lightMagic.rotation);
-    }
-    private void OnBecameInvisible()
-    {
-        Destroy(go);
-=======
-        Instantiate(lightballPrefab, gameObject.transform.position, gameObject.transform.rotation);
->>>>>>> 268c0c40a10535beb43bb79ad253d9eec49f9462
+        Instantiate(lightballPrefab, lightMagic.position, lightMagic.rotation);
     }
 }
 
--- Game Studio Semester Project/Assets/Scripts/magicmove.cs
+++ Game Studio Semester Project/Assets/Scripts/magicmove.cs
@@ -6,51 +6,18 @@ public class magicmove : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
-    private Vector2 start;
 
     void Start()
     {
-        start = transform.position;
         rb.velocity = transform.right * speed;
     }
 
-<<<<<<< HEAD
-    void onTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("enemy"))
-        {
-            collision.gameObject.SetActive(false);
-            //Destroy(gameObject);
-        }
+    //void onTriggerEnter2D(Collider2D collision)
+    //{
+    //    if(collision.gameObject.CompareTag("enemy"))
+    //    {
+    //        Destroy(gameObject);
+    //    }
 
-=======
-    private void Update()
-    {
-        //Didn't we want a range for our attack?
-
-        //if (gameObject.transform.position.x > start.x+5)
-        //{
-        //    Destroy(gameObject);
-        //}
-    }
-
-    //Attack dissapears on contact with enemy or leaving the screen
-    private void OnBecameInvisible()
-    {
-        Destroy(gameObject);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Map"))
-        {
-            Destroy(gameObject);
-        }
-        if (collision.gameObject.CompareTag("enemy"))
-        {
-            collision.gameObject.SetActive(false);
-            Destroy(gameObject);
-        }
->>>>>>> 268c0c40a10535beb43bb79ad253d9eec49f9462
-    }
+    //}
 }
